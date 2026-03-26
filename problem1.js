3efbd71c49599a4cd92424bc11816ab4d495bbad
saksham762007 <samrathore07062007@gmail.com>
2026-03-26 11:47:49 +0530

    changes

---
+++ problem1.js
@@ -0,0 +1,26 @@
+const readline = require("readline");
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+r1.question("Enter value: ", (answer) => {
+    let n = Number(answer);
+
+    let row = 1;
+
+    while (row <= n) {
+        let col = 1;
+
+        while (col <= row) {
+            process.stdout.write(row + " ");
+            col = col + 1;
+        }
+
+        console.log(); // same as cout << endl
+        row = row + 1;
+    }
+
+    r1.close();
+});

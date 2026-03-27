fb10406a81bd5f386ed1daa7d511e7f7c0fbcb98
saksham762007 <samrathore07062007@gmail.com>
2026-03-27 13:18:31 +0530

    changes

---
+++ p1.js
@@ -0,0 +1,13 @@
+const constants = require("constants");
+const readline = require("readline");
+
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+r1.question("enter the value: ", (answer) => {
+  let n = Number(answer);
+
+  
+});

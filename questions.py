2726f836ac8a55e11e1a2aec33fa5e35109bf97f
saksham762007 <samrathore07062007@gmail.com>
2026-08-09 01:35:00 +0530

    Add new Python file for palindrome check implementation

---
+++ questions.py
@@ -0,0 +1,11 @@
+n = 123
+
+num = n;
+count = 0;
+
+while num > 0:
+    ld = num % 10 
+    count = (result * 10) + ld
+    num = num // 10
+    
+return n == count
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,6 +1,31 @@
-        let col = 1;
+// Filter method
 
-        while (col <= row) {
-            process.stdout.write(col + " ");
-            col = col + 1;
-        }
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const evenNumbers = numbers.filter((num) => {
+//   return num % 2 === 0;
+// });
+
+// console.log(evenNumbers);
+
+
+// MAP method
+
+// const numbers = [1, 2, 3, 4];
+
+// const doubled = numbers.map((num) => {
+//   return num * 2;
+// });
+
+// console.log(doubled);
+
+
+
+// reduce method
+
+const arr = [1, 2, 3, 4];
+
+const sum = arr.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum); // 10
